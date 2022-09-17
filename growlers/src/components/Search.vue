b7114d76071e1c1519f0f39af86d2fd5684ebeb2
darqus <voladores@inbox.ru>
2022-09-17 03:29:41 +0300

    fix type of alcoholLimit

--- growlers/src/components/Search.vue
+++ growlers/src/components/Search.vue
@@ -3,7 +3,7 @@
     <h1 class="mb-2 font-bold text-2xl">Name</h1>
     <input v-model="searchText" class="focus:outline-none focus:ring focus:border-blue-300 px-4 py-3 leading-5 border rounded-md w-full">
     <h1 class="mb-2 font-bold text-2xl">Alcohol</h1>
-    <input type="range" v-model="alcoholLimit" min="0" max="15" class="w-full">
+    <input type="range" v-model.number="alcoholLimit" min="0" max="15" class="w-full">
   </div>
 </template>
 
